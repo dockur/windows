21ea7584a4a99ec46e9defc826fafb63f158cf30
Kroese <kroese@users.noreply.github.com>
2024-01-14 20:44:08 +0100

    fix: Remove progress (#8)

--- src/install.sh
+++ src/install.sh
@@ -9,13 +9,6 @@ ARGUMENTS="-tpmdev emulator,id=tpm0,chardev=chrtpm -device tpm-tis,tpmdev=tpm0 $
 BASE="$VERSION.iso"
 [ -f "$STORAGE/$BASE" ] && return 0
 
-# Check if running with interactive TTY or redirected to docker log
-if [ -t 1 ]; then
-  PROGRESS="--progress=bar:noscroll"
-else
-  PROGRESS="--progress=dot:giga"
-fi
-
 DEST="$STORAGE/drivers.img"
 [ ! -f "$DEST" ] && cp /run/drivers.iso "$DEST"
 
@@ -32,7 +25,7 @@ rm -f "$SCRIPT"
 
 [ ! -f "$STORAGE/tmp/$BASE" ] && error "Failed to download $VERSION.iso from the Microsoft servers!" && exit 66
 
-info "Modifying ISO to remove keypress requirement during boot..."
+info "Customizing ISO to remove keypress requirement during boot..."
 
 7z x "$BASE" -ounpack
 genisoimage -b boot/etfsboot.com -no-emul-boot -c BOOT.CAT -iso-level 4 -J -l -D -N -joliet-long -relaxed-filenames -v -V "Custom" -udf -boot-info-table -eltorito-alt-boot -eltorito-boot efi/microsoft/boot/efisys_noprompt.bin -no-emul-boot -o "$STORAGE/tmp/$BASE.tmp" -allow-limited-size unpack
