acec08d363ed16b4235b32158975ee7df896ca24
Kroese <kroese@users.noreply.github.com>
2024-01-14 14:44:24 +0100

    feat: Init script

---
+++ src/init.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+echo "❯ Starting Windows for Docker v$(</run/version)..."
+echo "❯ For support visit https://github.com/dockur/windows"
+
+cd /run
+
+. reset.sh      # Initialize system
+. install.sh    # Get bootdisk
+. disk.sh       # Initialize disks
+. display.sh    # Initialize graphics
+. network.sh    # Initialize network
+. boot.sh       # Configure boot
+. proc.sh       # Initialize processor
+. config.sh     # Configure arguments
+
+trap - ERR
+
+if [[ "${DISPLAY,,}" == "web" ]]; then
+  websockify -D --web /usr/share/novnc/ 8006 localhost:5900 2>/dev/null
+fi
+
+info "Booting image using $VERS..."
+
+[[ "$DEBUG" == [Yy1]* ]] && set -x
+exec qemu-system-x86_64 ${ARGS:+ $ARGS}
