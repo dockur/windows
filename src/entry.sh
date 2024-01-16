2acc6518f5c4f2a1f6605a789c655b961203d952
Kroese <kroese@users.noreply.github.com>
2024-01-16 19:27:23 +0100

    fix: Launch websockify

--- src/entry.sh
+++ src/entry.sh
@@ -7,9 +7,6 @@ echo "❯ For support visit https://github.com/dockur/windows"
 export DISPLAY=web
 export BOOT_MODE=windows
 
-ln -sfn /usr/share/novnc/vnc_lite.html /usr/share/novnc/index.html
-websockify -D --web /usr/share/novnc/ 8006 localhost:5900 2>/dev/null
-
 cd /run
 
 . reset.sh      # Initialize system
@@ -23,8 +20,12 @@ cd /run
 
 trap - ERR
 
+ln -sfn /usr/share/novnc/vnc_lite.html /usr/share/novnc/index.html
+websockify -D --web /usr/share/novnc/ 8006 localhost:5900 2>/dev/null
+
 mkdir -p /tmp/emulated_tpm
-swtpm socket -t -d --tpmstate dir=/tmp/emulated_tpm --ctrl type=unixio,path=/tmp/emulated_tpm/swtpm-sock --log level=1 --tpm2
+swtpm socket -t -d --tpmstate dir=/tmp/emulated_tpm --ctrl \
+  type=unixio,path=/tmp/emulated_tpm/swtpm-sock --log level=1 --tpm2
 
 info "Booting Windows using $VERS..."
 
