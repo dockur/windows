ed8e00b2b879b987febcef973c5c5437eb548b9b
Kroese <kroese@users.noreply.github.com>
2024-05-19 19:03:08 +0200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -656,7 +656,7 @@ updateXML() {
   fi
 
   if [ -n "$PASSWORD" ]; then
-    pass=$(printf '%s' "{$PASSWORD}Password" | iconv -f utf-8 -t utf-16le | base64)
+    pass=$(printf '%s' "${PASSWORD}Password" | iconv -f utf-8 -t utf-16le | base64)
     admin=$(printf '%s' "${PASSWORD}AdministratorPassword" | iconv -f utf-8 -t utf-16le | base64)
     sed -i "s/<Value>password<\/Value>/<Value>$admin<\/Value>/g" "$asset"
     sed -i "s/<PlainText>true<\/PlainText>/<PlainText>false<\/PlainText>/g" "$asset"
