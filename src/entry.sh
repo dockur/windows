49ca7da5fc86c058eac66340154d53de3a4c3331
Kroese <kroese@users.noreply.github.com>
2024-06-02 21:07:02 +0200

    Update entry.sh

--- src/entry.sh
+++ src/entry.sh
@@ -23,7 +23,8 @@ cd /run
 
 trap - ERR
 
-info "Booting ${APP}${BOOT_DESC}..."
+version=$(qemu-system-aarch64 --version | head -n 1 | cut -d '(' -f 1 | awk '{ print $NF }')
+info "Booting ${APP}${BOOT_DESC} using QEMU v$version..."
 
 { qemu-system-x86_64 ${ARGS:+ $ARGS} >"$QEMU_OUT" 2>"$QEMU_LOG"; rc=$?; } || :
 (( rc != 0 )) && error "$(<"$QEMU_LOG")" && exit 15
