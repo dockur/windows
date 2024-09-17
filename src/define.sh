315213c7a1d81869a91914f3fe668951270d35ed
Kroese <kroese@users.noreply.github.com>
2024-09-17 22:47:18 +0200

    fix: Use IDE CD-ROM instead of SCSI for 24H2

--- src/define.sh
+++ src/define.sh
@@ -2333,6 +2333,9 @@ setMachine() {
       if ! prepare2k3 "$iso" "$dir" "$desc"; then
         error "Failed to prepare $desc ISO!" && return 1
       fi ;;
+    "win2025"* | "win11x64-iot"* | "win11x64-ltsc"* )
+      # Temporary: Use IDE CD to workaround install bug
+      MEDIA_TYPE="ide" ;;
   esac
 
   return 0
