eb8775bec7606cc931513c3c1842a1ef824b0c87
ncheng89 <48044818+ncheng89@users.noreply.github.com>
2024-12-26 15:22:40 +0800

    Support more shared directories
    Control the sharing of more directories through environment variables instead of hard-coding more shared directories

--- src/samba.sh
+++ src/samba.sh
@@ -86,8 +86,14 @@ share="/data"
 
 addShare "$share" "Data" "Shared" || error "Failed to create shared folder!"
 
-[ -d "/data2" ] && addShare "/data2" "Data2" "Shared"
-[ -d "/data3" ] && addShare "/data3" "Data3" "Shared"
+IFS=',' read -r -a dirs <<< "${SHARED_DIRS:-}"
+# Traverse the directory and create shares
+for dir in "${dirs[@]}"; do
+  if [ -d "$dir" ]; then
+    dir_name=$(basename "$dir")
+    addShare "$dir" "$dir_name" "Shared $dir_name" || error "Failed to create shared folder for $dir!"
+  fi
+done
 
 if ! smbd; then
   error "Samba daemon failed to start!"
