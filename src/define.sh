277bb27e7004e3405221610eb769e83db5b976bd
Kroese <kroese@users.noreply.github.com>
2025-03-05 08:25:06 +0100

    Update define.sh

--- src/define.sh
+++ src/define.sh
@@ -715,6 +715,7 @@ getMido() {
     "win10x64-enterprise-eval" )
       size=5550497792
       sum="ef7312733a9f5d7d51cfa04ac497671995674ca5e1058d5164d6028f0938d668"
+      url="https://software-static.download.prss.microsoft.com/dbazure/988969d5-f34g-4e03-ac9d-1f9786c66750/19045.2006.220908-0225.22h2_release_svc_refresh_CLIENTENTERPRISEEVAL_OEMRET_x64FRE_en-us.iso"
       ;;
     "win10x64-enterprise-ltsc-eval" )
       size=4898582528
