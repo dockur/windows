97f40542b87491cdcbaa359d44e465cab57e44e0
Kroese <kroese@users.noreply.github.com>
2025-03-20 01:05:32 +0100

    Update entry.sh

--- src/entry.sh
+++ src/entry.sh
@@ -35,8 +35,7 @@ tail -fn +0 "$QEMU_LOG" 2>/dev/null &
 cat "$QEMU_TERM" 2> /dev/null | tee "$QEMU_PTY" | \
 sed -u -e 's/\x1B\[[=0-9;]*[a-z]//gi' \
 -e 's/failed to load Boot/skipped Boot/g' \
--e 's/0): Not Found/0)/g' &
-wait $! || :
+-e 's/0): Not Found/0)/g' & wait $! || :
 
 sleep 1 & wait $!
 [ ! -f "$QEMU_END" ] && finish 0
