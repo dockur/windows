6e08a5a2eab15fba75bed680c4dc34a67bb0fa8d
Gytis Stoškevičius <gytisstoskevicius@gmail.com>
2025-03-31 18:30:06 +0300

    Add debug

--- src/define.sh
+++ src/define.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -Eeuo pipefail
+set -Eeuox pipefail
 
 : "${WIDTH:=""}"
 : "${HEIGHT:=""}"
--- src/samba.sh
+++ src/samba.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -Eeuo pipefail
+set -Eeuox pipefail
 
 : "${SAMBA:="Y"}"
 
