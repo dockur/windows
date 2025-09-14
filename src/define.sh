299e6ce750e5876b55b1d239cd9f40ccbcdf1979
Kroese <kroese@users.noreply.github.com>
2025-09-14 19:46:14 +0200

    feat: Allow to skip download methods

--- src/define.sh
+++ src/define.sh
@@ -1313,7 +1313,7 @@ isMido() {
   local lang="$2"
   local sum
 
-  [[ "${MIDO:-}" != [Yy1]* ]] && return 1
+  [[ "${MIDO:-}" == [Nn]* ]] && return 1
 
   sum=$(getMido "$id" "en" "sum")
   [ -n "$sum" ] && return 0
@@ -1326,7 +1326,7 @@ isESD() {
   local id="$1"
   local lang="$2"
 
-  [[ "${ESD:-}" != [Yy1]* ]] && return 1
+  [[ "${ESD:-}" == [Nn]* ]] && return 1
 
   case "${id,,}" in
     "win11${PLATFORM,,}" | "win10${PLATFORM,,}" )
