1f63447090fa91f6fc45e50d0108840c4ccb73f2
Kroese <kroese@users.noreply.github.com>
2025-09-15 00:34:52 +0200

    fix: Debug message for downloading URL

--- src/mido.sh
+++ src/mido.sh
@@ -598,7 +598,7 @@ downloadFile() {
   fi
 
   info "$msg..."
-  [[ "$DEBUG" == [Yy1]* ]] && echo "Downloading $url"
+  [[ "$DEBUG" == [Yy1]* ]] && echo "Downloading: $url"
 
   { wget "$url" -O "$iso" -q --timeout=30 --no-http-keep-alive --user-agent "$agent" --show-progress "$progress"; rc=$?; } || :
 
