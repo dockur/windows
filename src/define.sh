4b9e389def299776c59467237ea423bee8678012
Kroese <kroese@users.noreply.github.com>
2025-09-15 14:11:20 +0200

    feat: Remove leading and trailing spaces from VERSION

--- src/define.sh
+++ src/define.sh
@@ -23,7 +23,8 @@ parseVersion() {
   if [[ "${VERSION}" == \"*\" || "${VERSION}" == \'*\' ]]; then
     VERSION="${VERSION:1:-1}"
   fi
-
+  
+  VERSION=`expr "$VERSION" : "^\ *\(.*[^ ]\)\ *$"`
   [ -z "$VERSION" ] && VERSION="win11"
 
   case "${VERSION,,}" in
