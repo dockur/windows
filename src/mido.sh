221f4feac379115ae359dd15fe215a23af3703ea
Kroese <kroese@users.noreply.github.com>
2025-09-22 21:38:24 +0200

    build: Update QEMU base image to v7.16 (#1419)

--- src/mido.sh
+++ src/mido.sh
@@ -404,7 +404,7 @@ getCatalog() {
   local file="catalog.cab"
 
   if [[ "${id,,}" == "win11"* ]] && ! isCompatible; then
-    # ARMv8.0 cannot run Windows 11 builds higher than 22631
+    # ARMv8.0 cannot run Windows 11 builds 24H2 and up.
     getBuild "$1" "$2" "22631.2861" && return 0
   fi
 
