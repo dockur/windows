a7c1936fd8e67eba34bcf0b6fddcc3dc1c637297
Kroese <kroese@users.noreply.github.com>
2026-06-02 23:27:15 +0200

    Update entry.sh

--- src/entry.sh
+++ src/entry.sh
@@ -32,18 +32,26 @@ trap - ERR
 version=$(qemu-system-x86_64 --version | head -n 1 | cut -d '(' -f 1 | awk '{ print $NF }')
 info "Booting ${APP}${BOOT_DESC} using QEMU v$version..."
 
-{ qemu-system-x86_64 ${ARGS:+ $ARGS} >"$QEMU_OUT" 2>"$QEMU_LOG"; rc=$?; } || :
-(( rc != 0 )) && error "$(<"$QEMU_LOG")" && exit 15
+[[ "$SHUTDOWN" != [Yy1]* ]] && exec qemu-system-x86_64 ${ARGS:+ $ARGS}
 
-terminal
-( sleep 30; boot ) &
-tail -fn +0 "$QEMU_LOG" --pid=$$ 2>/dev/null &
-cat "$QEMU_TERM" 2> /dev/null | tee "$QEMU_PTY" | \
-sed -u -e 's/\x1B\[[=0-9;]*[a-z]//gi' \
--e 's/\x1B\x63//g' -e 's/\x1B\[[=?]7l//g' \
--e '/^$/d' -e 's/\x44\x53\x73//g' \
--e 's/failed to load Boot/skipped Boot/g' \
--e 's/0): Not Found/0)/g' & wait $! || :
+if [ ! -t 1 ] || [ ! -c /dev/tty ]; then
+  qemu-system-x86_64 ${ARGS:+ $ARGS} &
+else
+  qemu-system-x86_64 ${ARGS:+ $ARGS} </dev/tty >/dev/tty &
+fi
+ 
+rc=0
+wait $! || rc=$?
+[ -f "$QEMU_END" ] && exit "$rc"
 
 sleep 1 & wait $!
-[ ! -f "$QEMU_END" ] && finish 0
+finish "$rc"
+
+#( sleep 30; boot ) &
+#tail -fn +0 "$QEMU_LOG" --pid=$$ 2>/dev/null &
+#cat "$QEMU_TERM" 2> /dev/null | tee "$QEMU_PTY" | \
+#sed -u -e 's/\x1B\[[=0-9;]*[a-z]//gi' \
+#-e 's/\x1B\x63//g' -e 's/\x1B\[[=?]7l//g' \
+#-e '/^$/d' -e 's/\x44\x53\x73//g' \
+#-e 's/failed to load Boot/skipped Boot/g' \
+#-e 's/0): Not Found/0)/g' & wait $! || :
