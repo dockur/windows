02c2538c2b75d1738583857d8c70968c92881a85
Kroese <kroese@users.noreply.github.com>
2026-06-04 11:58:35 +0200

    Update entry.sh

--- src/entry.sh
+++ src/entry.sh
@@ -35,7 +35,13 @@ info "Booting ${APP}${BOOT_DESC} using QEMU v$version..."
 if [ ! -t 1 ] || [ ! -c /dev/tty ]; then
   qemu-system-x86_64 ${ARGS:+ $ARGS} &
 else
-  qemu-system-x86_64 ${ARGS:+ $ARGS} </dev/tty >/dev/tty &
+  qemu-system-x86_64 ${ARGS:+ $ARGS} </dev/tty >/dev/tty | \
+  tee "$QEMU_PTY" | \
+  sed -u -e 's/\x1B\[[=0-9;]*[a-z]//gi' \
+  -e 's/\x1B\x63//g' -e 's/\x1B\[[=?]7l//g' \
+  -e '/^$/d' -e 's/\x44\x53\x73//g' \
+  -e 's/failed to load Boot/skipped Boot/g' \
+  -e 's/0): Not Found/0)/g' &
 fi
 
 pid=$!
