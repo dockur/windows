96277e5c09eaece7345c4c5b69d66b41aac1990b
Kroese <kroese@users.noreply.github.com>
2026-06-04 18:03:48 +0200

    Update entry.sh

--- src/entry.sh
+++ src/entry.sh
@@ -32,19 +32,24 @@ trap - ERR
 version=$(qemu-system-x86_64 --version | head -n 1 | cut -d '(' -f 1 | awk '{ print $NF }')
 info "Booting ${APP}${BOOT_DESC} using QEMU v$version..."
 
-qemu-system-x86_64 ${ARGS:+ $ARGS} > >(
-    tee "$QEMU_PTY" |
-    sed -u \
+log_pipe() {
+  tee "$QEMU_PTY" | sed -u \
     -e 's/\x1B\[[=0-9;]*[a-z]//gi' \
     -e 's/\x1B\x63//g' \
     -e 's/\x1B\[[=?]7l//g' \
     -e '/^$/d' \
     -e 's/\x44\x53\x73//g' \
     -e 's/failed to load Boot/skipped Boot/g' \
-    -e 's/0): Not Found/0)/g' ) &
+    -e 's/0): Not Found/0)/g'
+}
 
-pid=$!
-( sleep 30; boot ) &
+start() {
+  ( exec qemu-system-x86_64 ${ARGS:+ $ARGS} > >(log_pipe) ) &
+  pid=$!
+  ( sleep 30; boot ) &
+}
+
+start
 
 rc=0
 wait "$pid" || rc=$?
