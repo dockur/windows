11a16d34f873fe0e2815d4381125a0466a0e2cf4
Kroese <kroese@users.noreply.github.com>
2026-07-17 05:13:37 +0200

    fix: Show QEMU errors on unexpected exit (#1873)

--- src/power.sh
+++ src/power.sh
@@ -186,6 +186,12 @@ cleanupHelpers() {
 finish() {
 
   local reason=$1
+  local failed=0
+
+  if [ ! -f "$QEMU_END" ] && (( reason != 0 )); then
+    failed=1
+  fi
+
   touch "$QEMU_END"
 
   forceKillQemu "$reason"
@@ -200,7 +206,13 @@ finish() {
     warn "Timed out while waiting for $(app) to exit!"
   fi
 
-  (( reason != 1 )) && echo && echo "❯ Shutdown completed!"
+  echo
+
+  if (( failed == 0 )); then
+    echo "❯ Shutdown completed!"
+  else
+    error "QEMU exited unexpectedly!"
+  fi
 
   exit "$reason"
 }
