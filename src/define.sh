04984a49cb77ffe4a92e3a035e86be0b3775f68f
Kroese <kroese@users.noreply.github.com>
2026-07-21 17:19:13 +0200

    Update define.sh

--- src/define.sh
+++ src/define.sh
@@ -451,6 +451,18 @@ printVersion() {
     [[ "${PLATFORM,,}" != "x64" ]] && desc+=" for ${PLATFORM}"
   fi
 
+  case "${id,,}" in
+    *"-iot" | *"-iot-eval" )
+      desc+=" IoT"
+      ;;
+    *"-ltsc" | *"-ltsc-eval" )
+      desc+=" LTSC"
+      ;;
+    *"-enterprise" | *"-enterprise-eval" )
+      desc+=" Enterprise"
+      ;;
+  esac
+
   echo "$desc"
   return 0
 }
