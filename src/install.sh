c3256d524e5736092e3fe8050f43dd86ec5daa52
Kroese <kroese@users.noreply.github.com>
2026-07-25 15:46:21 +0200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -829,10 +829,13 @@ addFolder() {
   fi
 
   if [ -f "$file" ]; then
+
     if ! unix2dos -q "$file"; then
       error "Failed to convert $file to DOS format!"
       return 1
     fi
+
+    checkBatch "$file"
   fi
 
   return 0
