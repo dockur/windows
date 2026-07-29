785f3fcef11c3488523335f320913c8c1b59f494
Kroese <kroese@users.noreply.github.com>
2026-07-30 00:06:33 +0200

    Update answer.sh

--- src/answer.sh
+++ src/answer.sh
@@ -489,6 +489,29 @@ shiftDiskID() {
   return 0
 }
 
+hasAnswerFile() {
+
+  local id="$1"
+  local file="/run/assets/$id.xml"
+
+  [ -s "$file" ] && return 0
+
+  if [[ "${id,,}" == *"-eval" ]]; then
+    file="/run/assets/${id%-eval}.xml"
+    [ -s "$file" ] && return 0
+  fi
+
+  # Editions without a dedicated template can use the generic template.
+  case "${id,,}" in
+    "win7"* | "win8"* | "win10"* | "win11"* | "winvista"* | "win20"* )
+      file="/run/assets/${id%%-*}.xml"
+      [ -s "$file" ] && return 0
+      ;;
+  esac
+
+  return 1
+}
+
 stageAnswer() {
 
   local asset="$1"
