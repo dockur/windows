908da63c8c5262d02386d346df28cc4ad59923bd
Kroese <kroese@users.noreply.github.com>
2026-07-31 04:49:25 +0200

    Update install.sh

--- src/install.sh
+++ src/install.sh
@@ -1088,12 +1088,9 @@ addDrivers() {
   else
 
     local winpe="$src/$target"
-    local repository="$src/Drivers"
-
-    rm -rf "$winpe" "$repository" || return 1
-    mkdir -p "$winpe" "$repository" || return 1
+    rm -rf "$winpe" || return 1
+    mkdir -p "$winpe" || return 1
     cp -Lr "$dest/." "$winpe" || return 1
-    cp -Lr "$dest/." "$repository" || return 1
 
   fi
 
