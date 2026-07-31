b276810bd05ed8ac345b1ce64b495d54e5789e7d
Kroese <kroese@users.noreply.github.com>
2026-07-31 03:55:41 +0200

    Update answer.sh

--- src/answer.sh
+++ src/answer.sh
@@ -1903,7 +1903,10 @@ legacyInstall() {
     addLegacyDrivers "$dir" "$target" "$driver" "$arch" "$drivers" || return 1
   fi
 
-  disableAutoReboot "$target" || return 1
+  if [[ "${driver,,}" == "xp" || "${driver,,}" == "2k3" ]]; then
+    disableAutoReboot "$target" || return 1
+  fi
+
   setLegacyKey "$target" "$driver" "$arch" "$desc" || return 1
   validateProductKey "$KEY" || return 1
 
