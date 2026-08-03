aec0ea7f25f09e0859bb5cc833b1f3ae1783a5d1
Kroese <kroese@users.noreply.github.com>
2026-08-03 18:26:50 +0200

    Update define.sh

--- src/define.sh
+++ src/define.sh
@@ -927,40 +927,37 @@ getServerEditionID() {
 
 getEditionOrder() {
 
-  local -n geo_result="$2"
-  geo_result=()
+  local id="${1,,}"
 
-  case "${1,,}" in
+  case "$id" in
     "win20"* )
-      geo_result=(
-        "|default|@default"
-        "-datacenter|datacenter|datacenter datacenter-*"
-        "-datacenter-azure|datacenter|datacenter-azure"
-        "-enterprise|enterprise|enterprise enterprise-*"
-        "-web|web|web web-*"
-        "-foundation|foundation|foundation foundation-*"
-        "-essentials|essentials|essentials essentials-*"
-        "-standard-core|standard-core|standard-core standard-core-*"
-        "-datacenter-core|datacenter-core|datacenter-core datacenter-core-*"
-        "-datacenter-azure-core|datacenter-core|datacenter-azure-core"
-        "-enterprise-core|enterprise-core|enterprise-core enterprise-core-*"
-        "-web-core|web-core|web-core web-core-*"
+      printf '%s\n' \
+        "|default|@default" \
+        "-datacenter|datacenter|datacenter datacenter-*" \
+        "-datacenter-azure|datacenter|datacenter-azure" \
+        "-enterprise|enterprise|enterprise enterprise-*" \
+        "-web|web|web web-*" \
+        "-foundation|foundation|foundation foundation-*" \
+        "-essentials|essentials|essentials essentials-*" \
+        "-standard-core|standard-core|standard-core standard-core-*" \
+        "-datacenter-core|datacenter-core|datacenter-core datacenter-core-*" \
+        "-datacenter-azure-core|datacenter-core|datacenter-azure-core" \
+        "-enterprise-core|enterprise-core|enterprise-core enterprise-core-*" \
+        "-web-core|web-core|web-core web-core-*" \
         "-hv|hv|hv hv-*"
-      )
       ;;
     * )
-      geo_result=(
-        "-enterprise|enterprise|enterprise enterprise-*"
-        "-ultimate|ultimate|ultimate ultimate-*"
-        "|default|@default n pro pro-* professional professional-* business business-*"
-        "-iot|iot|iot iot-* enterprise-iot enterprise-iot-*"
-        "-ltsc|ltsc|ltsc ltsc-* enterprise-ltsc enterprise-ltsc-*"
-        "-education|education|education education-* pro-education pro-education-*"
-        "-home|home|home home-*"
-        "-home-premium|home|home-premium home-premium-*"
-        "-home-basic|home|home-basic home-basic-*"
+      printf '%s\n' \
+        "-enterprise|enterprise|enterprise enterprise-*" \
+        "-ultimate|ultimate|ultimate ultimate-*" \
+        "|default|@default n pro pro-* professional professional-* business business-*" \
+        "-iot|iot|iot iot-* enterprise-iot enterprise-iot-*" \
+        "-ltsc|ltsc|ltsc ltsc-* enterprise-ltsc enterprise-ltsc-*" \
+        "-education|education|education education-* pro-education pro-education-*" \
+        "-home|home|home home-*" \
+        "-home-premium|home|home-premium home-premium-*" \
+        "-home-basic|home|home-basic home-basic-*" \
         "-starter|starter|starter starter-*"
-      )
       ;;
   esac
 
