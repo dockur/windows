fe335223ed0912fe9491a757d2be85165f1b6d3a
Kroese <kroese@users.noreply.github.com>
2026-08-03 21:37:24 +0200

    Update answer.sh

--- src/answer.sh
+++ src/answer.sh
@@ -72,7 +72,7 @@ stageAnswer() {
     error "Failed to enable the Windows configuration set!"
     return 1
   fi
- 
+
   validateGeneratedXML "$answer" || return 1
 
   if [ -z "${CUSTOM_XML:-}" ]; then
@@ -1856,9 +1856,9 @@ setConfigurationXML() {
   local asset="$1"
 
   local ns="urn:schemas-microsoft-com:unattend"
+  local setup='/*[local-name()="unattend"]/*[local-name()="settings" and @pass="windowsPE"]/*[local-name()="component" and @name="Microsoft-Windows-Setup"]'
   local userdata="$setup/*[local-name()='UserData']"
   local config="$setup/*[local-name()='UseConfigurationSet']"
-  local setup='/*[local-name()="unattend"]/*[local-name()="settings" and @pass="windowsPE"]/*[local-name()="component" and @name="Microsoft-Windows-Setup"]' 
   local setup_count config_count config_value userdata_count result_count tmp
 
   [ -s "$asset" ] || return 1
