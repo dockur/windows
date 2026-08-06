0fc9e5596b487e574be5424532f64d8a6a18a96f
Kroese <kroese@users.noreply.github.com>
2026-08-06 23:06:39 +0200

    Update entry.sh

--- src/entry.sh
+++ src/entry.sh
@@ -25,8 +25,8 @@ cd /run
 . samba.sh      # Configure samba
 . boot.sh       # Configure boot
 . proc.sh       # Initialize processor
-. power.sh      # Configure shutdown
 . balloon.sh    # Initialize ballooning
+. power.sh      # Configure shutdown
 . config.sh     # Configure arguments
 . finish.sh     # Finish initialization
 
