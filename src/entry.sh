f4ce9e25f323c440a46aeca8bc7eda0b27a3fde0
Kroese <kroese@users.noreply.github.com>
2026-08-10 02:03:32 +0200

    Update entry.sh

--- src/entry.sh
+++ src/entry.sh
@@ -16,7 +16,7 @@ cd /run
 . define.sh     # Define versions
 . mido.sh       # Download Windows
 . answer.sh     # Setup answer files
-. legacy.sh     # Legacy versions
+. legacy.sh     # Legacy installation
 . image.sh      # Detect image files
 . install.sh    # Run installation
 . disk.sh       # Initialize disks
