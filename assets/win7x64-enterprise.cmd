8436fa0bec63b085a140d274205312974bc5263e
Kroese <kroese@users.noreply.github.com>
2026-09-03 19:42:23 +0200

    Update win7x64-enterprise.cmd

--- assets/win7x64-enterprise.cmd
+++ assets/win7x64-enterprise.cmd
@@ -16,7 +16,7 @@ exit /b 2
 rem Install the VMWare display driver before Windows Setup's final reboot.
 certutil.exe -addstore -f Root "%SystemRoot%\Drivers\vmsvga\vm3d.cer" >nul 2>&1
 certutil.exe -addstore -f TrustedPublisher "%SystemRoot%\Drivers\vmsvga\vm3d.cer" >nul 2>&1
-pnputil.exe -i -a "%SystemRoot%\Drivers\vmsvga\vm3d.inf"
+start "" /wait /b pnputil.exe -i -a "%SystemRoot%\Drivers\vmsvga\vm3d.inf" >nul 2>&1
 
 exit /b 0
 
