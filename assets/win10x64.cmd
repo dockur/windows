9be87977d16b0f24ea7836eb19ee96d0ab276b60
Kroese <kroese@users.noreply.github.com>
2026-09-04 03:49:33 +0200

    Update win10x64.cmd

--- assets/win10x64.cmd
+++ assets/win10x64.cmd
@@ -37,9 +37,9 @@ reg.exe unload "HKU\mount"
 reg.exe add "HKLM\Software\Policies\Microsoft\Windows\CloudContent" /v "DisableCloudOptimizedContent" /t REG_DWORD /d 1 /f
 reg.exe add "HKLM\Software\Policies\Microsoft\Windows\CloudContent" /v "DisableWindowsConsumerFeatures" /t REG_DWORD /d 1 /f
 reg.exe add "HKLM\Software\Policies\Microsoft\Windows\CloudContent" /v "DisableConsumerAccountStateContent" /t REG_DWORD /d 1 /f
+
 rem Set Network Location to Home
 reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows NT\CurrentVersion\NetworkList\Signatures\FirstNetwork" /v Category /t REG_DWORD /d 1 /f
-
 
 rem Install the VMWare display driver before Windows Setup's final reboot.
 certutil.exe -addstore -f Root "%SystemRoot%\Drivers\vmsvga\vm3d.cer" >nul 2>&1
