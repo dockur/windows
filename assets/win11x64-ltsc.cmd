b3553859704006616fbb068ce7b5c11c489b67a8
Kroese <kroese@users.noreply.github.com>
2026-09-04 03:24:10 +0200

    Update win11x64-ltsc.cmd

--- assets/win11x64-ltsc.cmd
+++ assets/win11x64-ltsc.cmd
@@ -6,10 +6,15 @@ set "SETUP_STARTED=%SCRIPT_DIR%setup.started"
 set "SETUP_COMPLETE=%SCRIPT_DIR%setup.complete"
 
 if "%~1"=="" goto setup
+if /i "%~1"=="pe" goto pe
 if /i "%~1"=="setup" goto setup
 if /i "%~1"=="logon" goto logon
 if /i "%~1"=="specialize" goto specialize
 exit /b 2
+
+:pe
+
+exit /b 0
 
 :specialize
 
@@ -19,6 +24,7 @@ certutil.exe -addstore -f TrustedPublisher "%SystemRoot%\Drivers\vmsvga\vm3d.cer
 start "" /wait /b pnputil.exe -i -a "%SystemRoot%\Drivers\vmsvga\vm3d.inf" >nul 2>&1
 
 exit /b 0
+
 :setup
 if exist "%SETUP_COMPLETE%" exit /b 0
 
